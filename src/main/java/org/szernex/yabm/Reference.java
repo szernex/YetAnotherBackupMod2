0c1d006cd18de8a4ed371b6a814ca1d5da4d34d1
Szernex <szernex@gmail.com>
2015-06-28 13:14:27 +0200

    project initialization

---
+++ src/main/java/org/szernex/yabm/Reference.java
@@ -0,0 +1,8 @@
+package org.szernex.yabm;
+
+public class Reference
+{
+	public static final String MOD_ID = "YABM";
+	public static final String MOD_NAME = "YetAnotherBackupMod";
+	public static final String VERSION = "2.0";
+}
