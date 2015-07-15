a076a047d2744532cd70efdfaaac9e52dc2b8eae
Szernex <szernex@gmail.com>
2015-07-15 22:34:52 +0200

    * bumped version to v1.7.10-2.0.2 * refactored BackupManagingHelper.consolidateBackups to take max_count and backup_dir as parameters so it can also be used for persistent backups * changed some logging messages to be easier to read

--- src/main/java/org/szernex/yabm2/handler/ConfigHandler.java
+++ src/main/java/org/szernex/yabm2/handler/ConfigHandler.java
@@ -20,6 +20,7 @@ public class ConfigHandler
 	public static boolean persistentBackups =   true;
 	public static String persistentPath =       "../backups/persistent";
 	public static int maxBackupCount =          8;
+	public static int maxPersistentCount =      14;
 
 	public static void init(File file)
 	{
@@ -44,11 +45,12 @@ public class ConfigHandler
 		backupInterval = configuration.getInt("backupInterval", category, backupInterval, 0, Integer.MAX_VALUE, "Interval for backups in minutes. Useful for singleplayer.\nSet this to 0 to enable backupSchedule instead.");
 		backupSchedule = configuration.getStringList("backupSchedule", category, backupSchedule, "The times for when to do backups in 24h format. Useful for servers.\nIf backupInterval is >0 then this setting has no effect.");
 		compressionLevel = configuration.getInt("compressionLevel", category, compressionLevel, 0, 9, "The Zip compression level to use to create backup archives (0 = no compression, 9 = maximum compression).");
-		backupPrefix = configuration.getString("backupPrefix", category, backupPrefix, "The prefix for the created Zip files. Archives will be formatted as '$prefix_worldname_timestamp.zip'.");
+		backupPrefix = configuration.getString("backupPrefix", category, backupPrefix, "The prefix for the created Zip files. Archives will be formatted as '$prefix_[persistent_]worldname_timestamp.zip'.");
 		backupPath = configuration.getString("backupPath", category, backupPath, "The path where to store the backups. Can be a relative or absolute path.");
 		persistentBackups = configuration.getBoolean("persistentBackups", category, persistentBackups, "Enables persistent backups which are done once per day and are excluded from automatic consolidation if enabled.");
 		persistentPath = configuration.getString("persistentPath", category, persistentPath, "Same as backupPath but for persistent backups.");
-		maxBackupCount = configuration.getInt("maxBackupCount", category, maxBackupCount, 0, Integer.MAX_VALUE, "The number of non-persistent backups to keep. If the total backup count exceeds this number the oldest backups will be deleted.\nA value of 0 disables backup consolidation.");
+		maxBackupCount = configuration.getInt("maxBackupCount", category, maxBackupCount, 0, Integer.MAX_VALUE, "The number of normal backups to keep. If the total backup count exceeds this number the oldest backups will be deleted.\n0 = disabled.");
+		maxPersistentCount = configuration.getInt("maxPersistentCount", category, maxPersistentCount, 0, Integer.MAX_VALUE, "The number of persistent backups to keep. If the total backup count exceeds this number the oldest backups will be deleted.\n0 = disabled.");
 
 		if (configuration.hasChanged())
 		{
